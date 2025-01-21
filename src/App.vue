ccc006ee7f4523481b723dc601d39778abae5620
norbert-gemes <gemes.norbert@students.jedlik.eu>
2025-01-21 12:25:36 +0100

    commit2

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
-import { AppHeader } from "./components/AppHeader.vue";
+import AppHeader from './components/AppHeader.vue';
 </script>
 
 <template>
-  <AppHeader></AppHeader>
+  <AppHeader/>
   <RouterView/>
 </template>
 
 <style scoped>
 </style>
+
+
+<!-- https://github.com/MeriLevente/L11_blog_frontend -->
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+<template>
+  <div class="header-bg">
+      <span class="header-title">BLOG</span>
+  </div>
+</template>
+
+<style scoped>
+</style>
